e900bf0f231152653a672e9a60c4c55b68cc6010
Philip Norton <phil.norton@weareaccess.co.uk>
2018-10-12 22:11:11 +0100

    Removing gallery functionality to allow the sposor logos to be clicked on without opening a gallery thingy.

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,37 +57,6 @@
     }).triggerHandler('resize.ie-flexbox-fix');
   }
 
-  // Gallery.
-  $window.on('load', function () {
-    var $gallery = $('.gallery');
-
-    $gallery.poptrox({
-      baseZIndex: 10001,
-      useBodyOverflow: false,
-      usePopupEasyClose: false,
-      overlayColor: '#1f2328',
-      overlayOpacity: 0.65,
-      usePopupDefaultStyling: false,
-      usePopupCaption: true,
-      popupLoaderText: '',
-      windowMargin: 50,
-      usePopupNav: true
-    });
-
-    // Hack: Adjust margins when 'small' activates.
-    breakpoints.on('>small', function () {
-      $gallery.each(function () {
-        $(this)[0]._poptrox.windowMargin = 50;
-      });
-    });
-
-    breakpoints.on('<=small', function () {
-      $gallery.each(function () {
-        $(this)[0]._poptrox.windowMargin = 5;
-      });
-    });
-  });
-
   // Section transitions.
   if (browser.canUse('transition')) {
     var on = function () {
